d020a8a689af9c9c22eb2493a4fd1d5518def3ba
Vincent Simon <simonvince@eisti.eu>
2012-12-21 12:43:51 +0100

    correction commit precedent

--- app/backend/module/eleve/EleveController.class.php
+++ app/backend/module/eleve/EleveController.class.php
@@ -7,14 +7,15 @@
  * 
  */
 class EleveController extends PandaController {
+
    public function index() {
-      $this->setWindowTitle('Accueil élève');      
+      $this->setWindowTitle('Accueil élève');
    }
-   
-   public function perso() {      
+
+   public function perso() {
       $this->setWindowTitle('Informations personnelles');
    }
-   
+
    public function promotion() {
       if (PandaRequest::getExists('promo')) {
          $this->setWindowTitle('Gestion de la promotion ' . PandaRequest::get('promo'));
@@ -24,19 +25,20 @@ class EleveController extends PandaController {
          $this->setWindowTitle('Gestion des promotions');
       }
    }
-   
+
    public function matieres() {
-       if(PandaRequest::getExists('promo') && PandaRequest::getExists('module') && PandaRequest::getExists('matiere')) {
+      if (PandaRequest::getExists('promo') && PandaRequest::getExists('module') && PandaRequest::getExists('matiere')) {
          $this->setWindowTitle('Gestion de ' . PandaRequest::get('matiere'));
          $this->page()->addVar('matiere', PandaRequest::get('matiere'));
+      }
    }
-   
-   public function notes () {
-      
-   } 
-   
-   
-   public function moyenness() {
+
+   public function notes() {
       
    }
+
+   public function moyennes() {
+      
    }
+
+}
