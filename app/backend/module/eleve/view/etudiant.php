2973f9cec5bc53758c81e0c547ec67c5639fcfcc
Vincent Simon <simonvince@eisti.eu>
2013-01-15 13:26:24 +0100

    modif elevecontroller, modules (promo, perso=>etudiant) suppression module disciplines et des roots

---
+++ app/backend/module/eleve/view/etudiant.php
@@ -0,0 +1,10 @@
+<?php if ($manageNotes) : ?>
+<?php endif; ?>
+<ul>
+   <h2>Voir mes notes par matière</h2>
+   <li><a href="/eleve/perso/notes">Notes par matière</a></li>
+   <h2>Voir mes moyennes par matière</h2>
+   <li><a href="/eleve/perso/moyennes">Moyennes par matière</a></li>
+</ul>
+
+   
